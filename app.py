a59436e8a56ffe4a7a13657ac6fe09ce2099d78f
Fogbodu1 <79920970+Fogbodu1@users.noreply.github.com>
2021-06-15 22:15:40 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import scrape_mars
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/phone_app"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_mission"
 mongo = PyMongo(app)
 
 
